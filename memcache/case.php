380dc9fce6615e194a87f36b795e0e340492d5ca
root <root@localhost.localdomain>
2017-03-21 05:21:16 -0400

    read

--- memcache/case.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-return array(
-    'MEMCACHE_DEFAULT' => array('host' => '127.0.0.1', 'port' => 11211)
-);
--- memcache/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once(__DIR__ . '/case.php');
-require_once(__DIR__.'/MyMemcached.class.php');
-$mem = MyMemcached::instance()->getMemcache();
-$key = "test";
-$t = $mem->set($key, 1);
-var_dump($t);
